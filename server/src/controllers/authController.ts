4316d694a8b9eb807ae1b2e41e66eeec66bec6e9
mohamedazizelheni <mohamedaziz.elheni@esprit.tn>
2025-02-20 20:02:27 +0400

    add role in token

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -53,10 +53,9 @@ export const login = async (req: Request, res: Response) => {
         role: user.role,
       },
     };
-
     // Sign token
     const token = jwt.sign(payload, process.env.JWT_SECRET as string, { expiresIn: '1h' });
-    res.json({ token });
+    res.json({ token, role: user.role });
   } catch (err) {
     console.error('Login error:', err);
     res.status(500).json({ message: 'Server error' });

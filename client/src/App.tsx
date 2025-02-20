070dbe6b14eb52df387287f8b2c5c52142df415c
mohamedazizelheni <mohamedaziz.elheni@esprit.tn>
2025-02-20 21:23:13 +0400

    install and configure Tailwind CSS

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+import Navbar from './components/Navbar';
+import KycSubmission from './pages/KycSubmission';
+import Login from './pages/Login';
+import AdminDashboard from './pages/AdminDashboard';
+import Register from './pages/Register';
+import PrivateRoute from './components/PrivateRoute';
+import Home from './pages/Home';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthProvider>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route
+            path="/admin"
+            element={
+              <PrivateRoute requiredRole="Admin">
+                <AdminDashboard />
+              </PrivateRoute>
+            }
+          />
+          <Route path="/kyc" element={<PrivateRoute requiredRole="User"><KycSubmission /></PrivateRoute>} />
+          </Routes>
+      </Router>
+    </AuthProvider>
   );
-}
+};
 
 export default App;
